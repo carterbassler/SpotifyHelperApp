c1837bbc3d29f2985299c20c3de64853e5bd0770
Carter Bassler <carter@Carters-MacBook-Pro.local>
2023-01-09 11:43:19 -0500

    middleware fix attempt 6

--- middleware.js
+++ middleware.js
@@ -1,23 +1,18 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
-  const url = req.nextUrl.clone()
-  url.pathname = '/login'
-  // token will exist if the user is logged in
-  const token = await getToken({ req, secret: process.env.JWT_SECRET })
-  const { pathname } = req.nextUrl
+    const token = await getToken({ req, secret: process.env.JWT_SECRET })
 
-  // ALlow the request if the following is true:
-  // 1) it's a request to next-auth session
-  // 2) the token exists
+    const { pathname } = req.nextUrl
 
-  if (pathname.includes('/api/auth') || token) {
-    return NextResponse.next()
-  }
+    if (pathname.includes('/api/auth') || token) {
+        return NextResponse.next();
+    }
 
-  // redirect to login page if the user is not logged in and they are requesting a protected routeing a protected route
-  if (!token && pathname !== url.pathname) {
-    return NextResponse.rewrite(url)
-  }
+    if (!token && pathname !== '/login') {
+        const url = req.nextUrl.clone()
+        url.pathname = '/login'
+        return NextResponse.redirect(url)
+    }
 }
