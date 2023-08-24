f3b06c4fbd0707a6f2a01b2d683a8801409aaaf6
Carter Bassler <carter@Carters-MacBook-Pro-2.local>
2023-08-24 13:07:45 -0400

    changed name of app

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -16,7 +16,7 @@ function MainPage({}: Props) {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="px-5 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-        Carter's <span className="text-green-500">Music Helper</span> App
+        <span className="text-green-500">Vibify</span>
       </h1>
       <h1 className='text-3xl md:text-5xl lg:text-6xl font-semibold scroll-px-10 text-white'>
                     <span>{text}</span>
